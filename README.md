ddb561abb3f8c0d4dfbbc7907208664184d94c08
altie122 <77561831+altie122@users.noreply.github.com>
2024-06-25 22:10:44 -0600

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Altie122's YouTube Stats
+
+This is a simple project that collects stats from my YouTube channel that runs nightly.
+
+Needed environment variables:
+- YOUTUBE_API_KEY
+- CHANNEL_ID
---
+++ scripts/collect-stats.ts
@@ -0,0 +1,84 @@
+import { google, youtube_v3 } from 'googleapis';
+import { writeFile } from 'node:fs/promises';
+import pRetry from 'p-retry';
+
+const retry: typeof pRetry = (fn, opts) =>
+  pRetry(fn, {
+    onFailedAttempt: (e) =>
+      console.log(
+        `Attempt ${e.attemptNumber} failed. There are ${e.retriesLeft} retries left.\n`,
+        e.message
+      ),
+    ...opts,
+  });
+
+class YouTubeStatsCollector {
+  #apiKey: string;
+  #youtube: youtube_v3.Youtube;
+
+  constructor(apiKey: string) {
+    this.#apiKey = apiKey;
+    this.#youtube = google.youtube({
+      version: 'v3',
+      auth: this.#apiKey,
+    });
+  }
+
+  async run(channelId: string) {
+    const videos = await this.#getAllVideos(channelId);
+    console.log('Processing data...');
+
+    const videoStats = await Promise.all(videos.map(video => this.#getVideoStats(video.id)));
+
+    console.log('Writing to disk...');
+    await this.#writeData(videoStats);
+    console.log('Mission complete!');
+  }
+
+  async #getAllVideos(channelId: string, pageToken = ''): Promise<{ id: string }[]> {
+    const request = async () => {
+      const response = await this.#youtube.search.list({
+        channelId,
+        part: 'id',
+        maxResults: 50,
+        pageToken,
+        type: 'video',
+      });
+      return response.data;
+    };
+
+    const data = await retry(request);
+    const videos = data.items?.map(item => ({ id: item.id.videoId })) || [];
+    const nextPageToken = data.nextPageToken;
+
+    if (nextPageToken) {
+      const nextVideos = await this.#getAllVideos(channelId, nextPageToken);
+      videos.push(...nextVideos);
+    }
+
+    return videos;
+  }
+
+  async #getVideoStats(videoId: string) {
+    const request = async () => {
+      const response = await this.#youtube.videos.list({
+        id: videoId,
+        part: 'statistics, snippet',
+      });
+      return response.data.items?.[0];
+    };
+
+    return await retry(request);
+  }
+
+  async #writeData(data: any) {
+    const filePath = './published/videoStats.json';
+    await writeFile(filePath, JSON.stringify(data), 'utf8');
+    console.log(`Data written to ${filePath}`);
+  }
+}
+
+const apiKey = process.env.YOUTUBE_API_KEY!;
+const channelId = process.env.CHANNEL_ID!;
+const collector = new YouTubeStatsCollector(apiKey);
+await collector.run(channelId);
